e6844d33063f2a1918c1b3eccb3cf458f7409338
aboudirai <raiaboudi@yahoo.com>
2019-09-21 19:42:33 -0400

    basic graphics

---
+++ src/Game.java
@@ -0,0 +1,117 @@
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+	
+	//8:42:31 LEFT OFF
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static final String NAME = "Untitled Game";
+	public static final int HEIGHT = 240;
+	public static final int WIDTH = HEIGHT * 16/9;
+	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	private boolean running = false;
+	private int tickCount;
+	
+	public void start() {
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void stop() {
+		running = false;
+	}
+	
+	public void run() {
+		long lastTime = System.nanoTime();
+		double unprocessed = 0;
+		double nsPerTick = 1000000000.0 / 60.0;
+		int frames = 0;
+		int ticks = 0;
+		long lastTimer1 = System.currentTimeMillis();
+		
+		while (running) {
+			long now = System.nanoTime();
+			unprocessed += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+			while(unprocessed >= 1) {
+				ticks++;
+				tick();
+				unprocessed -= 1;
+				shouldRender = true;
+			}
+			
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(shouldRender) {
+				frames++;
+				render();
+			}
+			
+			 if(System.currentTimeMillis() - lastTimer1 > 1000) {
+				lastTimer1 += 1000;
+				System.out.println(frames + "fps");
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+	
+	public void tick() {
+		tickCount++;
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = i + tickCount; 
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.setPreferredSize(new Dimension(WIDTH*2, HEIGHT*2));
+		game.setMaximumSize(new Dimension(WIDTH*2, HEIGHT*2));
+		game.setMinimumSize(new Dimension(WIDTH*2, HEIGHT*2));
+		
+		
+		JFrame frame = new JFrame(NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(game);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		game.start();
+		
+	}
+	
+	
+	
+}
